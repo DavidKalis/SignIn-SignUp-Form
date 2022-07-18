dc69068772f69924959d5a68e5db8373a9d3842c
DavidKalis <dawek977@gmail.com>
2022-07-18 06:00:36 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const signUpBtn = document.getElementById("signUp");
+const signInBtn = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpBtn.addEventListener("click", ()=> {
+    container.classList.add("righ-panel-active");
+});
+
